010b2f32d467fdbb96696ae2924e7f59f8710882
vvakame <vvakame+dev@gmail.com>
2010-10-30 21:23:36 +0900

    コマンドラインから簡単に署名付きapkが作成できるようになった

--- mvn.sh
+++ mvn.sh
@@ -2,3 +2,6 @@ mvn install
 mvn com.jayway.maven.plugins.android.generation2:maven-android-plugin:deploy
 mvn clean package -P sign -Dkeystore=./test.keystore -Dstorepass=hogehoge -Dkeypass=hogehoge -Dalias=test
 
+settings.xml設定後
+mvn install -Psign
+
