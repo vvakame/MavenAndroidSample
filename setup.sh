b9d8c6498d9d600375805e060e993f496859245a
vvakame <vvakame+dev@gmail.com>
2010-10-30 21:08:46 +0900

    Eclipseにimportするところまで

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 android create project --target 4 --name HelloMaven --path ./HelloMaven --activity HelloMaven --package com.example.hellomaven
+rm -r bin build.xml build.properties libs
 keytool -genkey -v -keystore test.keystore -alias test -keyalg RSA -validity 10000
 
