e301f442580084225aec7ba2fb75c10a77dcad8d
vvakame <vvakame+dev@gmail.com>
2010-10-30 22:15:44 +0900

    testプロジェクトの作成 Conversion to Dalvik format failed with error 1 が出るまで

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 android create project --target 4 --name HelloMaven --path ./HelloMaven --activity HelloMaven --package com.example.hellomaven
 rm -r bin build.xml build.properties libs
 keytool -genkey -v -keystore test.keystore -alias test -keyalg RSA -validity 10000
+mkdir test
+cd test
+android create test-project --path ./test --main ./ --name HelloMavenTest
 
