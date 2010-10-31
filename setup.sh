fe396a99d3f00c42d02008de7f1b66862a85aa9f
vvakame <vvakame+dev@gmail.com>
2010-10-31 22:21:58 +0900

    がっつりと構成変更

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
-android create project --target 4 --name HelloMaven --path ./HelloMaven --activity HelloMaven --package com.example.hellomaven
+# mkdir HelloMaven
+# cd HelloMaven
+android create project --target 4 --name hellomaven --path ./main --activity HelloMaven --package com.example.hellomaven
+cd main
 rm -r bin build.xml build.properties libs
-keytool -genkey -v -keystore test.keystore -alias test -keyalg RSA -validity 10000
+cd ../
 mkdir test
 cd test
-android create test-project --path ./test --main ./ --name HelloMavenTest
+android create test-project --path ./ --main ../main --name hellomaventest
+rm -r bin build.xml build.properties libs
+cd ../
+
+keytool -genkey -v -keystore test.keystore -alias test -keyalg RSA -validity 10000
 
